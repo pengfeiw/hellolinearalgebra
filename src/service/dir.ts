61be6d1c5c37e1e38325900718f744249a5d74d1
wangpengfei <2601339907@qq.com>
2021-10-25 20:39:00 +0800

    chore: delete unimportant files

--- src/service/dir.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-
-const BLOG_DIR = path.resolve(process.cwd(), "./src/docs/blogs");
-const TUTORIAL_DIR = path.resolve(process.cwd(), "./src/docs/tutorial");
-
-export {BLOG_DIR, TUTORIAL_DIR}
