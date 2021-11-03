b140874f97e44d0d836aa0d41daf57315f85e93d
wangpengfei <2601339907@qq.com>
2021-11-03 15:56:36 +0800

    chore: do nothing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import type {AppProps} from "next/app"
 import {MDXProvider} from "@mdx-js/react";
 import {ChakraProvider, extendTheme} from "@chakra-ui/react";
-import {ColorModeScript} from "@chakra-ui/react";
 import {theme as defaultTheme} from "src/ui/theme";
 import "polyfill-object.fromentries";
 import {mdxComponents} from "src/components/article/mdx";
