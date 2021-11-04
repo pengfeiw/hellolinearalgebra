67d78fd26b62017165fc9122f8e79710719efaec
wangpengfei <2601339907@qq.com>
2021-11-04 17:32:33 +0800

    chore: update doc

--- src/layouts/homeLayout.tsx
+++ src/layouts/homeLayout.tsx
@@ -26,7 +26,7 @@ const Layout = ({children, frontMatter}: React.PropsWithChildren<{frontMatter: F
                 <H1>{frontMatter.title}</H1>
                 <Text mb="15px">
                     这是一本关于入门计算机图形学的线性代数基础教程。主要介绍向量、行列式和矩阵的几何意义，如果你对图形学感兴趣，却缺乏一些数学知识，
-                    又没有时间去学习其他复杂的线性代数中繁杂的计算，那么你来对地方了，这本书应该非常适合你。本教程内容如下。
+                    又没有时间去学习其他复杂的线性代数中繁杂的计算，那么你来对地方了，这本书应该非常适合你，本教程包含以下章节内容。
                 </Text>
                 {
                     frontMatter.allPosts.map((post: {path: string; title: string;}) => (
